ac86c5f8a34c8e1d58ce0775508e6c628df21f23
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-11-02 12:17:07 +0530

    Styled the calculator

---
+++ 01_Calcultor/script.js
@@ -0,0 +1,13 @@
+// console.log("hello World");
+
+
+let btns = document.querySelectorAll(".btn_items")
+
+btns.forEach((btns)=>{
+    btns.addEventListener("click",()=>{
+        console.log("Hello World");
+        
+    })
+})
+
+// console.log(btns);
