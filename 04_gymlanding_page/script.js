ca582569add37d36677203c9593343fd93d00589
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-11-20 16:01:05 +0530

    updated code

---
+++ 04_gymlanding_page/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navItems = document.querySelector('.nav-items');
+  
+    menuToggle.addEventListener('click', () => {
+      navItems.classList.toggle('active');
+    });
+  });
+  
